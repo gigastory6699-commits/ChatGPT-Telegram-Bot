8dec1a6711d730534e5ec0886b84adbd8e363b9d
Mohamed108del <mohamed0155for@gmail.com>
2026-06-10 02:19:27 +0300

    fix: change streaming connection header to close to prevent hanging

--- proxy.py
+++ proxy.py
@@ -231,7 +231,7 @@ class FakeZeroProxy(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header("Content-Type", "text/event-stream; charset=utf-8")
                 self.send_header("Cache-Control", "no-cache")
-                self.send_header("Connection", "keep-alive")
+                self.send_header("Connection", "close")
                 self.end_headers()
                 
                 content = ""
